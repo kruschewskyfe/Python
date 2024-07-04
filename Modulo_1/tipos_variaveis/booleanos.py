e8c75385556f676744ba112363e7958adfca6142
Felipe Kruschewsky <kruschewsky.felipe@gmail.com>
2024-07-04 14:14:29 -0300

    Tipos de dados

---
+++ Modulo_1/tipos_variaveis/booleanos.py
@@ -0,0 +1,33 @@
+# Condição verdadeira
+True
+# Condição falsa
+False
+
+# Condicionais
+# se condicao
+# else = se não
+if True:
+    print("Bloco IF vai ser executado")
+else:
+    print("Bloco ELSE não será executado")
+
+# Operadores lógicos: and e or
+# AND
+if True and True:
+    print("Bloco AND será executado")
+
+if True and False:
+    print("Bloco AND não será executado")
+
+if False and False:
+    print("Bloco AND não será executado")
+
+# OR
+if True or False:
+    print("Bloco OR será executado")
+
+if False or False:
+    print("Bloco OR não será executado")
+
+if True or True:
+    print("Bloco OR será executado")
